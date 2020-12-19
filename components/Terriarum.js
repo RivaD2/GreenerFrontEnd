378f0acff71da5a1a36acbc4882721c3efe14685
RivaD2 <rivkadavidowski@gmail.com>
2020-12-19 15:26:32 -0800

    moved Terriarum logic out of explore into own component

---
+++ components/Terriarum.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import {
+    Image,
+    ScrollView,
+    TouchableOpacity
+  } from "react-native";
+import {Card, Badge, Button, Block, Text, Divider} from "../components";
+import {theme} from "../constants";
+
+export default class Terrarium extends React.Component {
+    render() {
+        // deconstructing props so I don't have to use this.props a trillion times
+        const {styles, name, plants, profile} = this.props;
+        return (
+            <Block>
+                <Block flex={false} row center space="between" style={styles.header}>
+                    <Text h1 bold>
+                      {name}
+                    </Text>
+                    <Button onPress={() => navigation.navigate("Settings")}>
+                        <Image source={profile.avatar} style={styles.avatar} />
+                    </Button>
+                </Block>
+                <ScrollView
+                showsVerticalScrollIndicator={false}
+                style={{ paddingVertical: theme.sizes.base * 2 }}
+                >
+                    <Divider margin={[theme.sizes.base, theme.sizes.base * 2]} />
+                    <Block flex={false} row space="between" style={styles.categories}>
+                    {plants.map(plant => (
+                        <TouchableOpacity
+                        key={plant.name}
+                        onPress={() => navigation.navigate("Explore", {plant})}
+                        >
+                            <Card center middle shadow style={styles.category}>
+                            <Badge
+                                margin={[0, 0, 15]}
+                                size={50}
+                                color="rgba(41,216,143,0.20)"
+                            >
+                            <Image source={plant.image} />
+                            </Badge>
+                            <Text medium height={20}>
+                              {plant.name}
+                            </Text>
+                            <Text gray caption>
+                              {plant.description} 
+                            </Text>
+                            </Card>
+                        </TouchableOpacity>
+                        ))}
+                    </Block>
+                </ScrollView>
+            </Block>
+        )
+    }
+}
